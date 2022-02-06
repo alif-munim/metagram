8a6ad5b3bd5d0891bf29cf6db50cdc36f2d0cb40
Alif Munim <alif.munim@ryerson.ca>
2022-02-05 20:33:26 -0500

    client build

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(express.static("build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
