e2e46096cf448104c39f40c381a586bd15506478
Alif Munim <alif.munim@ryerson.ca>
2022-02-05 19:59:37 -0500

    relative file structure

--- app.js
+++ app.js
@@ -147,8 +147,9 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 
 // ...
 // Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 app.listen(port, function() {
