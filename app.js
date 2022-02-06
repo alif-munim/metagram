d49ae53df4aab3c73f8ce2a13c1517d1fa615428
Alif Munim <alif.munim@ryerson.ca>
2022-02-05 19:29:40 -0500

    Add port

--- app.js
+++ app.js
@@ -140,4 +140,13 @@ app.use((err, req, res, next) => {
   });
 });
 
+var port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+app.listen(port, function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
+
+
 module.exports = app;
